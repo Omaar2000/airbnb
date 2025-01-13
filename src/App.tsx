3ada8dfeb15078342eb4394b0e1c4d369d65b743
Omardev288 <react1@ias-systems.com>
2025-01-13 13:50:27 +0200

    removing redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Users from "./pages/users/user";
 import Properties from "./pages/properties/properties";
@@ -11,12 +10,6 @@ import AddBooking from "./pages/bookings/addBooking";
 import AddPropertyPage from "./pages/properties/addProperty";
 import Navbar from "./components/navBar";
 
-// import { mockDataTeam, userColumns } from "./data/mockData";
-// import useUserStore from "./stores/useUserStore";
-// import Features from "./pages/features/Features";
-// import BoatsRequests from "./pages/BoatsRequests";
-// import BoatDetail from "./pages/BoatDetails";
-// import Users from "./pages/users/Users"
 function App() {
   return (
     <div style={{ transition: "all 2s" }}>
@@ -24,10 +17,6 @@ function App() {
         <Navbar />
         <div className="m-20">
           <Routes>
-            {/* <Route
-              path="/login"
-              element={token ? <Navigate to="/" /> : <Login />}
-              /> */}
             <Route path="users" element={<Users />} />
             <Route path="properties" element={<Properties />} />
             <Route
@@ -35,10 +24,6 @@ function App() {
               element={<AddPropertyPage />}
             />
             <Route path="properties/:id" element={<PropertyDetails />} />
-            {/* <Route
-              path="properties/addproperty"
-              element={<AddPropertyPage />}
-            /> */}
             <Route path="users/adduser" element={<AddUserPage />} />
             <Route path="users/edituser/:id" element={<EditUserPage />} />
             <Route path="bookings" element={<Bookings />} />
